085725ee359effee77625e94eac30eed1672a930
Grant <284885069@qq.com>
2026-02-24 12:16:11 +0800

    feat: 会话列表

--- apps/web/src/app/(main)/chat/components/index.tsx
+++ apps/web/src/app/(main)/chat/components/index.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@meta-1/design";
+import { List } from "./session/list";
+import { Messages } from "./session/messages";
+
 export const Page = () => {
-  return <div>chat page</div>;
+  return (
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel className="max-w-[360px] rounded-l-lg bg-(--bg-color-layout)/10" defaultSize={30}>
+        <List />
+      </ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel defaultSize={70}>
+        <Messages />
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
 };
---
+++ apps/web/src/app/(main)/chat/components/session/list/index.tsx
@@ -0,0 +1,42 @@
+import { ScrollArea } from "@meta-1/design";
+
+export const List = () => {
+  return (
+    <div className="h-0 flex-1 shrink-0">
+      <ScrollArea>
+        <div className="flex flex-col gap-2">
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+          <div>item</div>
+        </div>
+      </ScrollArea>
+    </div>
+  );
+};
